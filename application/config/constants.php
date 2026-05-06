9721a016e870a9a43ebde47fe95906101820abf6
Saurabh kumar <saurabhkumarsfd@gmail.com>
2026-05-06 14:01:32 +0530

    update

--- application/config/constants.php
+++ application/config/constants.php
@@ -85,5 +85,5 @@ defined('EXIT__AUTO_MIN') OR define('EXIT__AUTO_MIN', 9); // lowest automaticall
 defined('EXIT__AUTO_MAX') OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
 /* reCAPTCHA Keys */
-defined('RECAPTCHA_SITE_KEY') OR define('RECAPTCHA_SITE_KEY', '6Lfpx9ssAAAAABcSbbLNCpjFdxLYTZhqpOWnLDQO');
-defined('RECAPTCHA_SECRET_KEY') OR define('RECAPTCHA_SECRET_KEY', '6Lfpx9ssAAAAAIMNJjMkpwQ8jzux2nhbkYwsQGrq');
+defined('RECAPTCHA_SITE_KEY') OR define('RECAPTCHA_SITE_KEY', '6Lfk8dssAAAAAC0JvFcgivydtmrKqloH9H5xhkzN');
+defined('RECAPTCHA_SECRET_KEY') OR define('RECAPTCHA_SECRET_KEY', '6Lfk8dssAAAAAJtMtHQsPXH7sYxusv7J-Hle6bBX');
